5db77cf112da0f3c71dcbc46d755808f32aefb37
Maximilian Meister <mmeister@suse.de>
2017-04-05 13:14:54 +0200

    netstatconf.sh: add hidden ENV to set the RPC_PORT_PREFIX
    i decided to make it hidden, and not require to pass it explicitely
    to not break backwards compatibility
    
    without the ENV it falls back to 81 by default as before
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- netstatconf.sh
+++ netstatconf.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # bash intelligence <destination_app_json_path> <number_of_clusters> <name_prefix> <ws_server> <ws_secret>
 # you can also set the values as environment variables in upper case
-# - NUMBER_OF_CLUSTERS, NAME_PREFIX, WS_SERVER, WS_SECRET
+# - NUMBER_OF_CLUSTERS, NAME_PREFIX, WS_SERVER, WS_SECRET, RPC_PORT_PREFIX
 
 # sets up a eth-net-intelligence app.json for a local ethereum network cluster of nodes
 # - <number_of_clusters> is the number of clusters
@@ -16,12 +16,13 @@
 : ${NAME_PREFIX:=$2}
 : ${WS_SERVER:=$3}
 : ${WS_SECRET:=$4}
+: ${RPC_PORT_PREFIX:=81}
 
 echo -e "["
 
 for ((i=0;i<NUMBER_OF_CLUSTERS;++i)); do
     id=`printf "%02d" $i`
-    single_template="  {\n    \"name\"        : \"$NAME_PREFIX-$i\",\n    \"cwd\"         : \".\",\n    \"script\"      : \"app.js\",\n    \"log_date_format\"   : \"YYYY-MM-DD HH:mm Z\",\n    \"merge_logs\"    : false,\n    \"watch\"       : false,\n    \"exec_interpreter\"  : \"node\",\n    \"exec_mode\"     : \"fork_mode\",\n    \"env\":\n    {\n      \"NODE_ENV\"    : \"production\",\n      \"RPC_HOST\"    : \"localhost\",\n      \"RPC_PORT\"    : \"81$id\",\n      \"INSTANCE_NAME\"   : \"$NAME_PREFIX-$i\",\n      \"WS_SERVER\"     : \"$WS_SERVER\",\n      \"WS_SECRET\"     : \"$WS_SECRET\",\n    }\n  }"
+    single_template="  {\n    \"name\"        : \"$NAME_PREFIX-$i\",\n    \"cwd\"         : \".\",\n    \"script\"      : \"app.js\",\n    \"log_date_format\"   : \"YYYY-MM-DD HH:mm Z\",\n    \"merge_logs\"    : false,\n    \"watch\"       : false,\n    \"exec_interpreter\"  : \"node\",\n    \"exec_mode\"     : \"fork_mode\",\n    \"env\":\n    {\n      \"NODE_ENV\"    : \"production\",\n      \"RPC_HOST\"    : \"localhost\",\n      \"RPC_PORT\"    : \"$RPC_PORT_PREFIX$id\",\n      \"INSTANCE_NAME\"   : \"$NAME_PREFIX-$i\",\n      \"WS_SERVER\"     : \"$WS_SERVER\",\n      \"WS_SECRET\"     : \"$WS_SECRET\",\n    }\n  }"
 
     endline=""
     if [ "$i" -ne "$NUMBER_OF_CLUSTERS" ]; then
